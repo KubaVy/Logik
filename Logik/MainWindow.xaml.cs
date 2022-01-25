8c35b04177e6f9513551c95e45b6bab4239166c8
Jakub Vykydal <Vykydal.Jakub@stud.vassboskovice.cz>
2022-01-25 11:22:07 +0100

    kk

--- Logik/MainWindow.xaml.cs
+++ Logik/MainWindow.xaml.cs
@@ -20,14 +20,20 @@ namespace Logik
     /// </summary>
     public partial class MainWindow : Window
     {
+        Brush barva1;
+        Brush barva2;
+        Brush barva3;
+        Brush barva4;
         public MainWindow()
         {
             InitializeComponent();
+            NastavVariantu0();
         }
 
-        private void Ellipse_0_0_MouseDown(object sender, MouseButtonEventArgs e)
+        private void Ellipse_MouseDown(object sender, MouseButtonEventArgs e)
         {
             Ellipse ellipse = (Ellipse)sender;
+
             if (ellipse.Fill.ToString() == "#FFD9D9D9")
             {
                 ellipse.Fill = Brushes.Red;
@@ -52,16 +58,104 @@ namespace Logik
             {
                 ellipse.Fill = Brushes.Purple;
             }
-            else if (ellipse.Fill == Brushes.Purple)
+            else
             {
                 ellipse.Fill = Brushes.Red;
             }
-
-
-
-
-
         }
+
+      
+
+        private void NastavElipsuEnabled(Ellipse ellipse)
+        {
+            ellipse.IsEnabled = true;
+            ellipse.Fill = Brushes.LightGray;
+            ellipse.Stroke = Brushes.Black;
+            ellipse.Cursor = Cursors.Hand;
+        }
+        private void NastavElipsuDisabled(Ellipse ellipse)
+        {
+            ellipse.IsEnabled = false;
+            ellipse.Fill = Brushes.White;
+            ellipse.Stroke = Brushes.Gray;
+        }
+        private void NastavVariantu0()
+        {
+            NastavElipsuEnabled(Ellipse_0_0);
+            NastavElipsuEnabled(Ellipse_0_1);
+            NastavElipsuEnabled(Ellipse_0_2);
+            NastavElipsuEnabled(Ellipse_0_3);
+
+            NastavElipsuDisabled(Ellipse_1_0);
+            NastavElipsuDisabled(Ellipse_1_1);
+            NastavElipsuDisabled(Ellipse_1_2);
+            NastavElipsuDisabled(Ellipse_1_3);
+
+            NastavElipsuDisabled(Ellipse_2_0);
+            NastavElipsuDisabled(Ellipse_2_1);
+            NastavElipsuDisabled(Ellipse_2_2);
+            NastavElipsuDisabled(Ellipse_2_3);
+
+            NastavElipsuDisabled(Ellipse_3_0);
+            NastavElipsuDisabled(Ellipse_3_1);
+            NastavElipsuDisabled(Ellipse_3_2);
+            NastavElipsuDisabled(Ellipse_3_3);
+
+            NastavElipsuDisabled(Ellipse_4_0);
+            NastavElipsuDisabled(Ellipse_4_1);
+            NastavElipsuDisabled(Ellipse_4_2);
+            NastavElipsuDisabled(Ellipse_4_3);
+
+            NastavElipsuDisabled(Ellipse_5_0);
+            NastavElipsuDisabled(Ellipse_5_1);
+            NastavElipsuDisabled(Ellipse_5_2);
+            NastavElipsuDisabled(Ellipse_5_3);
+
+            NastavElipsuDisabled(Ellipse_6_0);
+            NastavElipsuDisabled(Ellipse_6_1);
+            NastavElipsuDisabled(Ellipse_6_2);
+            NastavElipsuDisabled(Ellipse_6_3);
+
+            NastavElipsuDisabled(Ellipse_7_0);
+            NastavElipsuDisabled(Ellipse_7_1);
+            NastavElipsuDisabled(Ellipse_7_2);
+            NastavElipsuDisabled(Ellipse_7_3);
+
+            NastavElipsuDisabled(Ellipse_8_0);
+            NastavElipsuDisabled(Ellipse_8_1);
+            NastavElipsuDisabled(Ellipse_8_2);
+            NastavElipsuDisabled(Ellipse_8_3);
+
             
+        }
+        private void NastavbarvyPC()
+        {
+            Random random = new Random();
+            int cislo = random.Next(1, 7);
+            if(cislo == 1)
+            {
+                barva1 = Brushes.Red;
+            }
+            else if (cislo == 2)
+            {
+                barva1 = Brushes.Green;
+            }
+            else if (cislo == 3)
+            {
+                barva1 = Brushes.Blue;
+            }
+            else if (cislo == 4)
+            {
+                barva1 = Brushes.Yellow;
+            }
+            else if (cislo == 5)
+            {
+                barva1 = Brushes.Orange;
+            }
+            else if (cislo == 6)
+            {
+                barva1 = Brushes.Purple;
+            }
+        }   
     }
 }
